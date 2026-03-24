719d96580bbff02b5e1a42dbe0857b784d0a640a
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2026-03-24 12:11:18 +0530

    Final cleanup: removed IDE and build files

--- Basics math/src/intro.java
+++
@@ -1,7 +0,0 @@
-public class intro {
-   public static void main(String[]args){
-    System.out.print("hello world \n welcome to java programming \n");
-    System.out.println("welcome to java programming");
-
-   }
-}
