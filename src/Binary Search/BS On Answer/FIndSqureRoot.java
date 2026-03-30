26698869ab40915587e2538c4e11ef80af11a338
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2026-03-30 09:45:12 +0530

    Binary on Ans

--- src/Binary Search/BS On Answer/FIndSqureRoot.java
+++ src/Binary Search/BS On Answer/FIndSqureRoot.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class FIndSqureRoot {
+public class FIndSqureRoot {
+    public static void main(String[] args) {
+        int n = 36;
+        int result = floorSqrt(n);
+        System.out.println("squre root is "  + result);
+    }
+    public static int floorSqrt(int n) {
+        int low = 0;
+        int high = n - 1;
+        int ans = 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid * mid <= n) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
 }
--- src/Binary Search/BS On Answer/NthRootofM.java
+++ src/Binary Search/BS On Answer/NthRootofM.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class NthRootofM {
+public class NthRootofM {
+    public static void main(String[] args) {
+        int N = 4;
+        int M = 69;
+        int result = NthRoot(N, M);
+        System.out.println("squre root is "  + result);
+    }
+    public static int NthRoot(int N, int M) {
+        int low = 1;
+        int high = M;
+        int ans = -1;
+
+        while(low<=high){
+            int mid = (low +high)/2;
+
+            double val = 1.0;
+            for (int i = 0; i < N; i++) {
+                val *= mid;
+                if (val > M) break; // early stop
+            }
+            if (val == M) {
+                return mid;
+            } else if (val < M) {
+                ans = mid;
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+
+        return -1;
+
+    }
 }
