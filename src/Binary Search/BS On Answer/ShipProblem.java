a4d3f5ab215892d9bda2247e3912aac9a038242b
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2026-03-31 12:05:44 +0530

    Binary on Ans

--- src/Binary Search/BS On Answer/ShipProblem.java
+++ src/Binary Search/BS On Answer/ShipProblem.java
@@ -1,2 +1,43 @@
+import java.util.Arrays;
+
 public class ShipProblem {
+    public static void main(String[] args) {
+        int[] weights = {1,2,3,4,5,6,7,8,9,10};
+        int d = 5;
+        int result = sipminimumdays(weights, d);
+        System.out.println(result);
+    }
+    private static int daysneeded(int [] weights, int capicty){
+        int days = 1;
+        int load = 0;
+        for(int w : weights){
+            if(load + w > capicty){
+                days++;
+                load = w;
+            }else {
+                load += w;
+            }
+
+        }
+        return days;
+    }
+    public static int sipminimumdays(int [] weights, int d){
+        int low = Arrays.stream(weights).max().getAsInt();
+        int high = Arrays.stream(weights).sum();
+
+
+        while (low<=high){
+            int mid = (low + high)/2;
+            int need = daysneeded(weights,mid);
+
+            if(need<=d){
+                high = mid-1;
+            }else {
+                low = mid+1;
+            }
+
+        }
+        return low;
+
+    }
 }
--- src/Binary Search/BS On Answer/SmallestDivisor.java
+++ src/Binary Search/BS On Answer/SmallestDivisor.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class SmallestDivisor {
+import java.util.Arrays;
+
+public class SmallestDivisor {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 5, 9};
+        int threshold = 6;
+
+        int result = smallestDivisor(nums, threshold);
+        System.out.println("Smallest Divisor: " + result);
+    }
+    private static int sumByD(int[] arr, int div) {
+        int sum = 0;
+        for(int num : arr){
+            sum += Math.ceil((double) num / div);
+        }
+        return sum;
+    }
+    public static int smallestDivisor(int[] nums, int threshold) {
+        if (nums.length > threshold) return -1;
+        int max = Arrays.stream(nums).max().getAsInt();
+        int low = 1;
+        int high = max;
+
+
+        while(low<=high){
+            int mid = (low + high)/2;
+            if((sumByD(nums, mid) <= threshold)){
+                high = mid -1;
+            }else{
+                low = mid +1;
+            }
+        }
+        return low;
+    }
 }
