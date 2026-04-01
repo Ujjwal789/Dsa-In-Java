729a609fdb339a8774269c20ea341a8be9fdbaf0
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2026-04-01 09:48:43 +0530

    Binary on Ans

--- src/Binary Search/BS On Answer/AgressiveCow.java
+++ src/Binary Search/BS On Answer/AgressiveCow.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class AgressiveCow {
+import java.util.Arrays;
+
+public class AgressiveCow {
+    public static void main(String[] args) {
+        int [] nums = {4, 2, 1, 3, 6};
+        int k = 2;
+        int result = aggressiveCows(nums, k);
+        System.out.println(result);
+    }
+    private static boolean possibleplace(int[] nums, int k, int d) {
+        int count = 1;
+        int last = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] - last >= d) {
+
+                count++;
+                last = nums[i];
+            }
+            if (count >= k) return true;
+        }
+        // Not possible to place all cows
+        return false;
+    }
+
+    public static int aggressiveCows(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int low = 1;
+        int high = nums[n-1] -nums[0];
+        int ans = 0;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if (possibleplace(nums, k, mid)) {
+                ans = mid;        // store answer
+                low = mid + 1;    // try bigger distance
+            } else {
+                high = mid - 1;   // reduce distance
+            }
+        }
+
+        return ans;
+    }
 }
--- src/Binary Search/BS On Answer/FindMissingKth.java
+++ src/Binary Search/BS On Answer/FindMissingKth.java
@@ -1,2 +1,22 @@
 public class FindMissingKth {
+    public static void main(String[] args) {
+        int [] arr = {2,3,4,7,11};
+        int k = 5;
+        int result = findKthPositive(arr,k);
+        System.out.println(result);
+    }
+    public static int findKthPositive(int[] arr, int k) {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int missing = arr[mid] - (mid+1);
+            if(missing<k){
+                low = mid+1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return k+high+1;
+    }
 }
