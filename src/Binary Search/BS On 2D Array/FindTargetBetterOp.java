36357672ea31da1953d698237536c20316531d24
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2026-04-02 13:57:25 +0530

    Binary on 2D Array

--- src/Binary Search/BS On 2D Array/FindTargetBetterOp.java
+++ src/Binary Search/BS On 2D Array/FindTargetBetterOp.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class FindTargetBetterOp {
+public class FindTargetBetterOp {
+    public static void main(String[] args) {
+        int [][] matrix = {{1,2,3,},{4,5,6},{7,8,9}};
+        int target = 8;
+        boolean result = searchMatrix(matrix,target);
+        System.out.println(result);
+
+    }
+    public static boolean binarySearch(int[] nums, int target){
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        while(low<high){
+            int mid = (low +high)/2;
+            if (nums[mid] == target)
+                return true;
+            else if (target > nums[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return false;
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i =0; i<m; i++){
+            for(int j =0; j<n; j++){
+                if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
+                    // Perform binary search on this row
+                    return binarySearch(matrix[i], target);
+                }
+            }
+        }
+        return false;
+
+    }
 }
--- src/Binary Search/BS On 2D Array/FindTargetInTwoD.java
+++ src/Binary Search/BS On 2D Array/FindTargetInTwoD.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class FindTargetInTwoD {
+public class FindTargetInTwoD {
+    public static void main(String[] args) {
+        int [][] matrix = {{1,2,3,},{4,5,6},{7,8,9}};
+        int target = 11;
+        boolean result = searchMatrix(matrix,target);
+        System.out.println(result);
+
+    }
+    public static boolean searchMatrix(int [][] matrix, int target){
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int low = 0;
+        int high = n*m-1;
+        while (low<high){
+            int mid = (low+high)/2;
+
+            int row = mid/m;
+            int col = mid%m;
+
+            if(matrix[row][col]==target){
+                return true;
+            } else if (matrix[row][col]>target) {
+                high = mid-1;
+                
+            }else {
+                low = mid +1;
+            }
+        }
+        return false;
+    }
 }
