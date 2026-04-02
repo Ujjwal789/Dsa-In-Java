7049f3b88e981c8caafa8fd1070a4f03a93e9246
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2026-04-02 13:57:51 +0530

    Binary on 2D Array

--- src/Binary Search/BS On 2D Array/FindTargetInTwoD.java
+++ src/Binary Search/BS On 2D Array/FindTargetInTwoD.java
@@ -8,26 +8,26 @@ public class FindTargetInTwoD {
     }
     public static boolean searchMatrix(int [][] matrix, int target){
 
-        int m = matrix.length;
-        int n = matrix[0].length;
+            int m = matrix.length;
+            int n = matrix[0].length;
 
-        int low = 0;
-        int high = n*m-1;
-        while (low<high){
-            int mid = (low+high)/2;
+            int low = 0;
+            int high = n*m-1;
+            while (low<high){
+                int mid = (low+high)/2;
 
-            int row = mid/m;
-            int col = mid%m;
+                int row = mid/m;
+                int col = mid%m;
 
-            if(matrix[row][col]==target){
-                return true;
-            } else if (matrix[row][col]>target) {
-                high = mid-1;
-                
-            }else {
-                low = mid +1;
+                if(matrix[row][col]==target){
+                    return true;
+                } else if (matrix[row][col]>target) {
+                    high = mid-1;
+
+                }else {
+                    low = mid +1;
+                }
             }
+            return false;
         }
-        return false;
-    }
 }
