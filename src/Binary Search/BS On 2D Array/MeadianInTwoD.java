522b4e9583870b0fd4da1dea142d88d7e5d3cf32
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2026-04-02 16:17:47 +0530

    Binary on 2D Array

--- src/Binary Search/BS On 2D Array/MeadianInTwoD.java
+++ src/Binary Search/BS On 2D Array/MeadianInTwoD.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MeadianInTwoD {
+    public static void main(String[] args) {
+
+        // Define the matrix
+        int[][] matrix = {
+                {1, 3, 5},
+                {2, 6, 9},
+                {3, 6, 9}
+        };
+
+        // Print the median
+        System.out.println(findMedian(matrix));
+    }
+    public static int findMedian(int[][] matrix) {
+        List<Integer> elements = new ArrayList<>();
+        for (int[] row : matrix) {
+            for (int val : row) {
+                elements.add(val);
+            }
+        }
+        Collections.sort(elements);
+        int n = elements.size();
+        return elements.get(n / 2);
+    }
 }
--- src/Binary Search/BS On 2D Array/PeakInTwoD.java
+++ src/Binary Search/BS On 2D Array/PeakInTwoD.java
@@ -1,2 +1,39 @@
 public class PeakInTwoD {
+
+        public  static void main(String[] args) {
+            int[][] mat = {
+                    {1, 2, 3, 1, 8},
+                    {2, 3, 4, 5, 6}
+            };
+
+            int[] res = findPeakGrid(mat);
+
+            System.out.println("Peak at: row = " + res[0] + ", col = " + res[1]);
+        }
+
+    public static int[] findPeakGrid(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int low = 0;
+        int high = m-1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            int row = 0;
+            for (int i = 0; i < n; i++) {
+                if (mat[i][mid] > mat[row][mid]) row = i;
+            }
+
+            int left = mid > 0 ? mat[row][mid - 1] : Integer.MIN_VALUE;
+            int right = mid < m - 1 ? mat[row][mid + 1] : Integer.MIN_VALUE;
+
+            if (mat[row][mid] > left && mat[row][mid] > right)
+                return new int[]{row, mid};
+            else if (left > mat[row][mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return new int[]{-1, -1};
+    }
 }
